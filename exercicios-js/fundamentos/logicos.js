acf87fe3b71d51c057f637de585ebc12b9753264
Bruno <bruno.olympio.ferreira@gmail.com>
2021-09-07 19:33:12 -0300

    Comitando 07/09 noite

---
+++ exercicios-js/fundamentos/logicos.js
@@ -0,0 +1,13 @@
+function compras(trabalho1, trabalho2){
+    const comprarSorvete = trabalho1 || trabalho2
+    const comrparTv50 = trabalho1 && trabalho2
+    //const comprarTv32 = !!(trabalho1 ^ trabalho2) //bitwise xor
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorvete // operador unario
+
+    return { comprarSorvete, comrparTv50, comprarTv32, manterSaudavel }
+}
+console.log(compras(true,true))
+console.log(compras(true,false))
+console.log(compras(false,true))
+console.log(compras(false,false))
