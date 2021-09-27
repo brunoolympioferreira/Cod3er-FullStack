622f22034b934e9194c263150a59f1dad954354e
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2021-09-27 18:13:11 -0300

    Create OO.js
    Comitando 27/09 tarde

---
+++ 5-objetos/OO.js
@@ -0,0 +1,16 @@
+//Codigo não executável
+
+// Procedural
+processamrento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco Passou a ser o objeto
