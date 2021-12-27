7e5b7bc23751790053bf3dc0dccad68108d3fdc8
Bruno Olympio <b.olympio@reply.com>
2021-12-27 16:07:25 -0300

    funcoes do node

---
+++ JsModerno/Node/PastaA/PastaB/AcesssarModulo.js
@@ -0,0 +1,11 @@
+const moduloA = require('../../ModuloA')
+console.log(moduloA.ola)
+
+const c = require('./PastaC')
+console.log(c.ola2)
+
+const http = require('http')
+http.createServer((req, res) => {
+    res.write('Bom dia!')
+    res.end()
+}).listen(8080)
